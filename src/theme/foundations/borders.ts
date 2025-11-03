ef96b02d94dcc0893fd8db2a480d88513b80b117
akira <academia@adminnoMacBook-Pro-2.local>
2025-11-03 09:19:02 +0900

    feat: bordersの中身の定義

--- src/theme/foundations/borders.ts
+++ src/theme/foundations/borders.ts
@@ -1,5 +1,28 @@
 export const borders = {
 
-
+    none: '0',
+    full: '9999px',
+  
+    // 2px
+    sm: '0.125rem',
+  
+    // 4px
+    base: '0.25rem',
+  
+    // 6px
+    md: '0.375rem',
+  
+    // 8px
+    lg: '0.5rem',
+  
+    // 12px
+    xl: '0.75rem',
+  
+    // 16px
+    '2xl': '1rem',
+  
+    // 20px
+    '3xl': '1.25rem',
+  
 };
   
